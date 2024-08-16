9c22b3d2592ad9d6c4f2cc4b2f47f1a60ee96b4f
NurTasin <nmuatasin2005@gmail.com>
2024-08-17 02:01:32 +0600

    Fixes the ES Module Require Error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    "reactStrictMode": false
+    "reactStrictMode": false,
+    transpilePackages: ['next-mdx-remote', 'shiki'],
 };
 
 export default nextConfig;
